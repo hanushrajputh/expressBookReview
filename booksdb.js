bb033041112e5400bb1f43b4c2408afa04143014
R Hanush Singh <148356740+hanushrajputh@users.noreply.github.com>
2026-03-26 20:58:26 +0530

    Create booksdb.js

---
+++ booksdb.js
@@ -0,0 +1,14 @@
+let books = {
+    "1": {
+        title: "Book One",
+        author: "Author One",
+        reviews: {}
+    },
+    "2": {
+        title: "Book Two",
+        author: "Author Two",
+        reviews: {}
+    }
+};
+
+module.exports = books;
