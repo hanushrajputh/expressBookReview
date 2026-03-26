3a7fbf6a1835e2b9f84cdfad7eead48bd77e88be
R Hanush Singh <148356740+hanushrajputh@users.noreply.github.com>
2026-03-26 21:49:48 +0530

    Update general.js

--- router/general.js
+++ router/general.js
@@ -1,43 +1,29 @@
 const axios = require('axios');
 
+// Task 11 - MUST USE AXIOS
+
 // Get all books
 async function getAllBooks() {
-    try {
-        const response = await axios.get('http://localhost:5000/');
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
+    const response = await axios.get('http://localhost:5000/');
+    return response.data;
 }
 
 // Get by ISBN
 async function getBookByISBN(isbn) {
-    try {
-        const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
+    const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+    return response.data;
 }
 
 // Get by Author
 async function getBooksByAuthor(author) {
-    try {
-        const response = await axios.get(`http://localhost:5000/author/${author}`);
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
+    const response = await axios.get(`http://localhost:5000/author/${author}`);
+    return response.data;
 }
 
 // Get by Title
 async function getBooksByTitle(title) {
-    try {
-        const response = await axios.get(`http://localhost:5000/title/${title}`);
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
+    const response = await axios.get(`http://localhost:5000/title/${title}`);
+    return response.data;
 }
 
 module.exports = {
